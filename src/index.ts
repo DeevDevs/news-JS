bbb7e76b0245c78439457c810a45b6f09258adf8
DeevDevs <dmitriy.vnuchkov@gmail.com>
2022-12-09 10:39:57 +0400

    feat: complete slider for sources. Add responsiveness. Also, add little responsiveness to the news block. Make the h1 fit the page in narrow screens.

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,21 @@ app.start();
 const arrowButtons = document.querySelectorAll('.btn-slide');
 const sourcesBlock = document.querySelector('.sources') as HTMLElement;
 let currentPosition = 0;
+let sourcesWidth: number;
+const windowWidth = window.innerWidth;
+if (windowWidth >= 1260) sourcesWidth = 1140;
+if (windowWidth < 1260 && windowWidth >= 980) sourcesWidth = 840;
+if (windowWidth < 980 && windowWidth >= 640) sourcesWidth = 500;
+if (windowWidth < 640) sourcesWidth = 210;
 function slideSources(e: Event) {
     const btn = e.target as HTMLElement;
     if (btn.classList.contains('slide-right')) {
         currentPosition = currentPosition === -2100 ? -2100 : (currentPosition -= 100);
-        sourcesBlock.style.transform = `translateX(${currentPosition}%)`;
     }
     if (btn.classList.contains('slide-left')) {
         currentPosition = currentPosition === 0 ? 0 : currentPosition + 100;
-        sourcesBlock.style.transform = `translateX(${currentPosition}%)`;
     }
+    sourcesBlock.style.transform = `translateX(${(currentPosition / 100) * sourcesWidth}px)`;
 }
 
 arrowButtons.forEach((btn) => {
--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ const baseConfig = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index2.html'),
+            template: path.resolve(__dirname, './src/index.html'),
             filename: 'index.html',
         }),
         new CleanWebpackPlugin(),
